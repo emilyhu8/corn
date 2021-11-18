58d63eccd82688fd63c7f54e6f6b20b39408348a
emilyhu8 <91348233+emilyhu8@users.noreply.github.com>
2021-11-18 16:30:25 -0500

    links are added but ugly

--- Cornspace/app/src/main/java/com/example/cornspace/LinksActivity.kt
+++ Cornspace/app/src/main/java/com/example/cornspace/LinksActivity.kt
@@ -10,6 +10,10 @@ class LinksActivity : AppCompatActivity() {
     private lateinit var link2: TextView
     private lateinit var link3: TextView
     private lateinit var link4: TextView
+    private lateinit var link5: TextView
+    private lateinit var link6: TextView
+    private lateinit var link7: TextView
+    private lateinit var link8: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_links)
@@ -17,6 +21,10 @@ class LinksActivity : AppCompatActivity() {
         link2=findViewById(R.id.link2)
         link3=findViewById(R.id.link3)
         link4=findViewById(R.id.link4)
+        link5=findViewById(R.id.link5)
+        link6=findViewById(R.id.link6)
+        link7=findViewById(R.id.link7)
+        link8=findViewById(R.id.link8)
 
     }
 }
