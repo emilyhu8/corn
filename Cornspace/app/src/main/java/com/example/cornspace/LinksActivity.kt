34381d9c537254bad1bdd308448a956d9e925e76
emilyhu8 <91348233+emilyhu8@users.noreply.github.com>
2021-11-21 16:08:20 -0500

    buttons with listeners

--- Cornspace/app/src/main/java/com/example/cornspace/LinksActivity.kt
+++ Cornspace/app/src/main/java/com/example/cornspace/LinksActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cornspace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.widget.Button
 import android.widget.TextView
 
 class LinksActivity : AppCompatActivity() {
@@ -14,6 +16,10 @@ class LinksActivity : AppCompatActivity() {
     private lateinit var link6: TextView
     private lateinit var link7: TextView
     private lateinit var link8: TextView
+    private lateinit var eventButton: Button
+    private lateinit var homeButton: Button
+    private lateinit var noteButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_links)
@@ -25,6 +31,24 @@ class LinksActivity : AppCompatActivity() {
         link6=findViewById(R.id.link6)
         link7=findViewById(R.id.link7)
         link8=findViewById(R.id.link8)
+
+        eventButton=findViewById(R.id.eventButton)
+        homeButton=findViewById(R.id.homeButton)
+        noteButton=findViewById(R.id.noteButton)
+
+        homeButton.setOnClickListener{
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        eventButton.setOnClickListener{
+            val intent= Intent(this, EventsActivity::class.java)
+            startActivity(intent)
+        }
+
+        noteButton.setOnClickListener{
+
+        }
 
     }
 }
